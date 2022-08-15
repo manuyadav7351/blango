57bf56a82ebd60c307b4f2eb86b43aa436a47fc3
manu yadav <manuyadav7351@gmail.com>
2022-08-15 17:29:37 +0000

    Finish Crispy Forms

--- blangowebapp/blangowebapp/settings.py
+++ blangowebapp/blangowebapp/settings.py
@@ -37,6 +37,8 @@ SESSION_COOKIE_SAMESITE = 'None'
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -130,3 +132,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
